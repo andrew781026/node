12892174531e22cec98f0eb70d6e29c60c8cb5e1
adrew <adrew@riko.com.tw>
2017-09-13 16:56:58 +0800

    修改 index-04.js

--- myblog/index-04.js
+++ myblog/index-04.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
 var articleRouter = require('./routes/article');
 var blogRouter = require('./routes/blog');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 
 app.set('views', path.join(__dirname, 'views'));// 设置存放模板文件的目录
 app.set('view engine', 'ejs');// 设置模板引擎为 ejs
